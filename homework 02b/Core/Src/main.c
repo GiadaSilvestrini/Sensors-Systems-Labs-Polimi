f6661a21f28450bd702c331411e06bd6b91852ac
Dell <Dell@192.168.1.58>
2024-05-14 08:03:47 +0200

    G

---
+++ homework 02b/Core/Src/main.c
@@ -0,0 +1,620 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef struct note{
+	int note;
+	int delay;
+}note;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define TIME_UNIT 1000
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim10;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+volatile int play;
+volatile int first_callback=1;
+volatile int delay_ended=0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM10_Init(void);
+static void setFrequency(int frequency)
+{
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 100;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = frequency -1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = (int)frequency/2 -1;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  HAL_TIM_MspPostInit(&htim1);
+}
+
+int getPeriod(char* note){
+	if(strcmp(note, "la")==0)
+		return 1909;
+	else if (strcmp(note, "la#")==0)
+			return 1802;
+	else if (strcmp(note, "si")==0)
+			return 1700;
+	else if (strcmp(note, "do")==0)
+			return 3206;
+	else if (strcmp(note, "do#")==0)
+			return 3032;
+	else if (strcmp(note, "re")==0)
+			return 2857;
+	else if (strcmp(note, "re5")==0)
+				return 1431;
+	else if (strcmp(note, "re#")==0)
+			return 2700;
+	else if (strcmp(note, "mi")==0)
+			return 2545;
+	else if (strcmp(note, "fa")==0)
+			return 2406;
+	else if (strcmp(note, "fa#")==0)
+			return 2270;
+	else if (strcmp(note, "sol")==0)
+			return 2142;
+	else if (strcmp(note, "sol#")==0)
+			return 2024;
+}
+struct note setNote (char* note, int delay){
+	struct note nota;
+	nota.note= getPeriod(note);
+	nota.delay= delay;
+	return nota;
+}
+struct note* compose_song(){
+
+}
+void playLondonBridge(){
+  struct note song[24];
+  song[0]= setNote("sol", (int)TIME_UNIT * 3/8);
+  song[1]=setNote("la", (int)TIME_UNIT * 1/8);
+  song[2]=setNote("sol", (int)TIME_UNIT * 1/4);
+  song[3]=setNote("fa", (int)TIME_UNIT * 1/4);
+  song[4]=setNote("mi", (int)TIME_UNIT * 1/4);
+  song[5]=setNote("fa", (int)TIME_UNIT * 1/4);
+  song[6]=setNote("sol", (int)TIME_UNIT * 1/2);
+  song[7]=setNote("re", (int)TIME_UNIT * 1/4);
+  song[8]=setNote("mi", (int)TIME_UNIT * 1/4);
+  song[9]=setNote("fa", (int)TIME_UNIT * 1/2);
+  song[10]=setNote("mi", (int)TIME_UNIT * 1/4);
+  song[11]=setNote("fa", (int)TIME_UNIT * 1/4);
+  song[12]=setNote("sol", (int)TIME_UNIT * 1/2);
+  song[13]=setNote("sol", (int)TIME_UNIT * 3/8);
+  song[14]=setNote("la", (int)TIME_UNIT * 1/8);
+  song[15]=setNote("sol", (int)TIME_UNIT * 1/4);
+  song[16]=setNote("fa", (int)TIME_UNIT * 1/4);
+  song[17]=setNote("mi", (int)TIME_UNIT * 1/4);
+  song[18]=setNote("fa", (int)TIME_UNIT * 1/4);
+  song[19]=setNote("sol", (int)TIME_UNIT * 1/2);
+  song[20]=setNote("re", (int)TIME_UNIT * 1/2);
+  song[21]=setNote("sol", (int)TIME_UNIT * 1/2);
+  song[22]=setNote("mi", (int)TIME_UNIT * 1/4);
+  song[23]=setNote("do", (int)TIME_UNIT * 3/4);
+
+  for(int i=0; i<sizeof(song)/sizeof(song[1]); i++){
+	  setFrequency(song[i].note);
+	  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+	  setTimer (song[i].delay);
+	  HAL_TIM_Base_Start_IT(&htim10);
+	  while(!delay_ended){}
+	  delay_ended=0;
+  }
+  first_callback=1;
+  delay_ended=0;
+}
+/*void playTequila(){
+	for(int i=0; i<3; i++){
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 2/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sfa", (int)TIME_UNIT * 1/8);
+		setNote("la", (int)TIME_UNIT * 2/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 4/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 2/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("la", (int)TIME_UNIT * 2/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 5/8);
+		HAL_Delay((int)TIME_UNIT*1/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 2/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sfa", (int)TIME_UNIT * 1/8);
+		setNote("la", (int)TIME_UNIT * 2/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 4/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("re", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 2/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("la", (int)TIME_UNIT * 2/8);
+		setNote("fa", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 1/8);
+		setNote("sol", (int)TIME_UNIT * 5/8);
+	}
+
+	setNote("sol", (int)TIME_UNIT * 4/8);
+	HAL_Delay((int)TIME_UNIT*1/8);
+	setNote("re", (int)TIME_UNIT * 1/8);
+	setNote("mi", (int)TIME_UNIT * 1/8);
+	setNote("sol", (int)TIME_UNIT * 1/8);
+	setNote("la#", (int)TIME_UNIT * 8/8);
+	setNote("sol", (int)TIME_UNIT * 4/8);
+	HAL_Delay((int)TIME_UNIT*1/8);
+	setNote("re", (int)TIME_UNIT * 1/8);
+	setNote("mi", (int)TIME_UNIT * 1/8);
+	setNote("sol", (int)TIME_UNIT * 1/8);
+	setNote("la#", (int)TIME_UNIT * 8/8);
+	setNote("sol", (int)TIME_UNIT * 4/8);
+	HAL_Delay((int)TIME_UNIT*1/8);
+	setNote("re", (int)TIME_UNIT * 1/8);
+	setNote("mi", (int)TIME_UNIT * 1/8);
+	setNote("sol", (int)TIME_UNIT * 1/8);
+	setNote("la#", (int)TIME_UNIT * 8/8);
+	setNote("sol", (int)TIME_UNIT * 8/8);
+	setNote("la", (int)TIME_UNIT * 1/8);
+	setNote("la", (int)TIME_UNIT * 1/8);
+	setNote("la", (int)TIME_UNIT * 1/8);
+	setNote("la", (int)TIME_UNIT * 1/8);
+	setNote("la", (int)TIME_UNIT * 2/8);
+	setNote("la", (int)TIME_UNIT * 2/8);
+	setNote("re5", (int)TIME_UNIT * 8/8);
+}
+*/
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	play = 1;
+}
+void setTimer(int delay)
+{
+  htim10.Instance = TIM10;
+  htim10.Init.Prescaler = 9999;
+  htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim10.Init.Period = (int)(8400-1)*delay;
+  htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+/* USER CODE BEGIN PFP */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim10){
+	if(first_callback==1){
+		first_callback=0;
+	}else{
+		first_callback=1;
+		delay_ended=1;
+		HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
+		HAL_TIM_Base_Stop_IT(htim10);
+	}
+}
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_TIM1_Init();
+  MX_TIM10_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  int index=0;
+  while (1)
+  {
+	 if(play==1){
+		 playLondonBridge();
+		 play=0;
+	 }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 100-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM10 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM10_Init(void)
+{
+
+  /* USER CODE BEGIN TIM10_Init 0 */
+
+  /* USER CODE END TIM10_Init 0 */
+
+  /* USER CODE BEGIN TIM10_Init 1 */
+
+  /* USER CODE END TIM10_Init 1 */
+  htim10.Instance = TIM10;
+  htim10.Init.Prescaler = 10000-1;
+  htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim10.Init.Period = 8400-1;
+  htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM10_Init 2 */
+
+  /* USER CODE END TIM10_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
